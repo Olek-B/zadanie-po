78e8d48fb6bfe1e161ba02cf72d01414d175a190
Olek-B <u57_alebla_waw@technischools.com>
2024-02-20 22:32:19 +0100

    warstwa logiki i persystencji zrobiona

--- logika.py
+++ logika.py
@@ -16,5 +16,24 @@ class meneger:
         if group not in self.allowedGroups:
             return("groupError")
         self.persystencja.write({"firstName":firstName,"lastName":lastName,"birthYear":birthYear,"group":group})
-    def get_user(id):
-        return self.persystencja.read(id)
+    def get_user(self, id):
+        if self.persystencja.check(id):
+            return self.persystencja.read(id)
+        return("idError")
+    def get_users(self):
+        return self.persystencja.get_users()
+    def remove_user(self,id):
+        if self.persystencja.check(id):
+            self.persystencja.remove_user(id)
+            return True
+        return("idError")
+    def change_user(self, id, firstName=None, lastName=None, birthYear=None,group=None):
+        if set(firstName) != self.allowedLetters and firstName != None:
+            return('nameError')
+        if set(lastName) != self.allowedLetters and lastName != None:
+            return("lastNameError")
+        if (birthYear.isdigit() or birthYear < 1900) and birthYear != None:
+            return("birthYearError")
+        if group not in self.allowedGroups and group != None:
+            return("groupError")
+        self.persystencja.change_user({"firstName":firstName,"lastName":lastName,"birthYear":birthYear,"group":group})
--- persystencja.py
+++ persystencja.py
@@ -3,13 +3,36 @@ import json
 class persystencja:
     def __init__(self, plik_persystencji):
         self.plik = plik_persystencji
+
     def write(dane):
         with open(self.plik,'rw') as f:
             data=json.load(f.read())
             data["users"][data["newest"]] = dane
             data["last"]=data["last"]+1
             f.write(data)
+
     def read(self,id):
         with open(self.plik, "r") as f:
             data=json.load(f.read())
             return data["users"][str(id)]
+
+    def get_users():
+        ret=[]
+        with open(self.plik, "r") as f:
+            data=json.load(f.read())
+            for i in data["users"].keys:
+                ret.append({i,data["users"][i]["firstName"],data["users"][i]["lastName"],data["users"][i]["birthDate"],data["users"][i]["group"]})
+        return ret
+
+    def check(id):
+        with open(self.plik, "r") as f:
+            data=json.load(f.read())
+            if str(id) in data["users"].keys:
+                return True
+            return False
+
+    def remove_user(id):
+        with open(self.plik, "r") as f:
+            data=json.load(f.read())
+            del data["users"][id]
+            f.write(data)
